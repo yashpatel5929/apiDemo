348f4f8bf7a498ae4ce3a8c99ba35d8356f1f76e
yashpatel5929 <yashp6765@gmail.com>
2022-08-05 17:33:31 +0530

    changes in api_interface class

--- app/src/main/java/com/example/apicalldemo/Network/ApiInterface.java
+++ app/src/main/java/com/example/apicalldemo/Network/ApiInterface.java
@@ -5,7 +5,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface ApiInterface {
-
+    //doing some changes in API_interface
     @GET("getmoviedetail")
     Call<Void> getMovieDetail(@Query("page_no") String page_no);
 }
